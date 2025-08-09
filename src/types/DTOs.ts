72a75ee3a476270bb9755275f9995d04a8639732
Nguyễn Đình Duy <ndduy269@gmail.com>
2025-08-09 22:19:33 +0700

    refactor the jobs query backend

--- src/types/DTOs.ts
+++ src/types/DTOs.ts
@@ -1,12 +1,23 @@
-import type {User} from "enigma/types/models";
+import type { User } from "enigma/types/models";
 
 export interface GenericResponse<T = never> {
     data?: T;
     error?: string;
     success?: string;
+    message?: string;
 }
 
 export interface PaginatedUsers {
     users: Array<User>;
     total: number;
+}
+
+export interface PageginatedResponse<T> {
+    items: T[];
+    meta: {
+        total: number;
+        page: number;
+        limit: number;
+        totalPages: number;
+    }
 }
